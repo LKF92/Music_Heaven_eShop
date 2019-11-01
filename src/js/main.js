5593681e9684946a46f753b118fccfbb2a8b0afd
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-11-01 15:58:55 +0100

    added progress bar

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-console.log('it works')
+document.addEventListener("scroll", () => {
+  let windowHeight = window.innerHeight;
+  let documentHeight = document.body.clientHeight;
+  let value = window.pageYOffset;
+  let progressBar = document.querySelector(".progressBar");
+
+  /* Set the max scrollable area */
+  let max = documentHeight - windowHeight;
+  progressBar.setAttribute("max", max);
+  progressBar.setAttribute("value", value);
+
+  document.onscroll = () => {
+    let value = window.pageYOffset;
+    progressBar.setAttribute("value", value);
+  };
+});
